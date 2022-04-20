44da3e1bccb063cd2068fbd3213da506d8d0ce66
Emuohwo <warriisaac@outlook.com>
2022-04-19 14:34:23 -1100

    Delete a Category

--- apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService, Category } from '@elobong/products'
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'admin-categories-list',
@@ -11,9 +12,30 @@ export class CategoriesListComponent implements OnInit {
 
   categories: Category[] = []
 
-  constructor(private categoriesService: CategoriesService) { }
+  constructor(
+    private categoriesService: CategoriesService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this._getCategories()
+  }
+
+  deleteCategory(categoryId: string) {
+    this.categoriesService.deleteCategory(categoryId).subscribe(res => {
+      this._getCategories();
+      this.messageService.add({
+        severity:'success', summary:'Success', detail: res.message
+      });
+    },
+    (error) => {
+      this.messageService.add({
+        severity:'error', summary:'Error', detail:'Category was not deleted, Please try again later'
+      });
+    })
+  }
+
+  private _getCategories(){
     this.categoriesService.getCategories().subscribe((cats) => {
       this.categories = cats;
     })
