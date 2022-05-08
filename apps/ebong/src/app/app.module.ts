89b49e1df34864175091c052def67cef76b33152
Emuohwo <warriisaac@outlook.com>
2022-05-08 12:56:36 -1100

    Add ui lib, banner

--- apps/ebong/src/app/app.module.ts
+++ apps/ebong/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { UiModule } from "@elobong/ui"
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -9,7 +10,6 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { UiModule } from '@elobong/ui';
 import {AccordionModule} from 'primeng/accordion';
 import { NavComponent } from './shared/nav/nav.component';
 import { ProductsModule } from '@elobong/products';
@@ -22,7 +22,8 @@ const routes: Routes = [
 @NgModule({
     declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent, NavComponent],
     imports: [BrowserModule, BrowserAnimationsModule, 
-        RouterModule.forRoot(routes), UiModule, AccordionModule, ProductsModule
+        RouterModule.forRoot(routes), AccordionModule, ProductsModule,
+        UiModule
         
     ],
     providers: [],
